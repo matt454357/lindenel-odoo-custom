440baf9b22db5de89778ec36b566c18d7bb8adff
Matt Taylor <matt454357@gmail.com>
2022-07-01 14:19:24 -0600

    add valve move filters; improve core ticket

--- valve_tracking/models/qb_invoice.py
+++ valve_tracking/models/qb_invoice.py
@@ -6,7 +6,7 @@ class QbInvoice(models.Model):
     _name = 'qb.invoice'
     _description = 'QuickBooks Invoice'
     _inherit = ['mail.thread']
-    _order = "name desc"
+    _order = "txn_date desc"
     _sql_constraints = [
         ('qb_ref_uniq', 'unique (qb_ref)', "QB TxnID must be unique"),
     ]
